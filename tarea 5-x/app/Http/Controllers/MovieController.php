0260d30141251aedda60528e84572ba88fcdac1c
Montgomery06 <54654239+Montgomery06@users.noreply.github.com>
2020-11-18 11:15:04 -0700

    Add files via upload

--- tarea 5-x/app/Http/Controllers/MovieController.php
+++ tarea 5-x/app/Http/Controllers/MovieController.php
@@ -80,8 +80,21 @@ class MovieController extends Controller
      * @param  \App\Models\Movie  $movie
      * @return \Illuminate\Http\Response
      */
-    public function edit(Movie $movie)
+    public function get(Movie $movie)
     {
+
+        if ($movie) {
+             return response()->json([
+                 'message' => 'Registro consultado correctamente',
+                 'code' => '200',
+                 'movie' => $movie
+             ]);
+         }
+         return response()->json([
+             'message' => 'Registro no encontrado',
+             'code' => '400',
+             'movie' => array()
+         ]);
         //
     }
 
@@ -94,6 +107,28 @@ class MovieController extends Controller
      */
     public function update(Request $request, Movie $movie)
     {
+
+        if ($movie) {
+             if ($movie->update($request->all())) {
+
+                  if ($request->hasFile('cover_file')) {
+
+                      $file = $request->file('cover_file');
+                     $file_name = 'cover_movie'.$movie->id.'.'.$file->getClientOriginalExtension();
+
+                      $path = $request->file('cover_file')->storeAs(
+                         'img', $file_name
+                     );
+
+                      $movie->cover = $file_name;
+                     $movie->save(); 
+
+                  }
+
+                  return redirect()->back();
+             }
+         }
+         return redirect()->back();
         //
     }
 
